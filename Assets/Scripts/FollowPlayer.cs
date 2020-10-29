18a3e6f0db6fc8a1013e4fff73df5fde2b6bc40d
Max Weis <maxweis@protonmail.com>
2020-10-29 13:59:04 +0100

    add map

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    private Transform playerPos;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerPos = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerPos.position.x,playerPos.position.y,transform.position.z);
+    }
+}
